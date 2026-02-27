008c1918752ae96911f33519b264d05f64ac43e2
stemarques2303 <ste.marques.2303@gmail.com>
2026-02-27 14:38:22 -0300

    aula 27-02

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ function App () {
   return(
     <div>
       <h1>Aula de React cm Vite JS</h1>
+      <h2>Estilização CSS</h2>
+      <h3>Eu não fiz nada</h3>
     </div>
   );
 }
---
+++ src/Sobre/page.jsx
@@ -0,0 +1,11 @@
+
+function Sobre () {
+    return(
+   <div>
+    <h1> Sobre</h1>
+    <p>Texto do meu SObre, Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci distinctio odit, voluptatibus deserunt eos ipsa facilis natus est vitae voluptate ratione animi commodi omnis, dignissimos assumenda saepe impedit vel? Vel.</p>
+   </div>
+    )
+}
+
+export default Sobre;
