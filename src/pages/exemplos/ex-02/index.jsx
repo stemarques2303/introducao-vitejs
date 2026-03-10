5f9f437888da050c7f0384f8f94dc001dc0e53bb
stemarques2303 <ste.marques.2303@gmail.com>
2026-03-10 11:44:09 -0300

    aula 10-03

--- src/pages/exemplos/ex-02/index.jsx
+++ src/pages/exemplos/ex-02/index.jsx
@@ -1,9 +1,8 @@
-
-import styles from './index.module.css';
+import './index.css'; 
 
 function Exemplo02 () {
     return (
-   <div className={styles.containerEx2}>
+   <div className="containerEx2">
     <h1>Exemplo 2</h1>
     <h2>Estilização CSS</h2>
     <h3>Uso de CSS em diferentes componentes</h3>
