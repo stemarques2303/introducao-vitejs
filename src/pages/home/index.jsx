1d85b79cd71af3c6b3a285167b5e323dab4b4eea
stemarques2303 <ste.marques.2303@gmail.com>
2026-03-13 14:40:17 -0300

    aula 13-03

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,27 @@
 
+import {useNavigate} from 'react-router-dom';
+import { Link } from "react-router";
+
 function Home () {
+
+    const navigate = useNavigate();
+
     return (
-   <div>
-    <h1>Aula de React com Vite JS</h1>
-    </div>
+     <div className= 'containerHome'>
+        <div className='containerCards'>
+          <h1>Aula de React com Vite JS</h1>
+
+           <button
+               onClick= {() =>navigate('/exeplo/1')}
+            >
+              Exemplo 1
+           </button>
+
+           <Link to= "/exemplo/2">Exemplo 2</Link>
+
+           </div>
+           
+        </div>
     )
 }
 
